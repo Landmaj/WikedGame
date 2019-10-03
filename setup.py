cbbbfe7ff83f5aa0c67487a9aab614f509b393fc
Michał Wieluński <michal@w-ski.dev>
2019-10-03 18:55:43 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ test_requirements = []
 setup(
     name="WikedGame",
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     install_requires=requirements,
     extras_require={"dev": test_requirements},
     license="GPL",
     author="Michał Wieluński",
     author_email="michal@w-ski.dev",
     description="Find the shortest path between two articles on Wikipedia.",
+    url="https://github.com/landmaj/WikedGame",
 )
