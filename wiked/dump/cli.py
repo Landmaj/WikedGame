11b9b1a62dc944e3e683b7517b07bf412345d2ff
Michał Wieluński <michal@w-ski.dev>
2019-10-07 21:37:36 +0200

    refactor CLI script

--- wiked/dump/cli.py
+++ wiked/dump/cli.py
@@ -8,42 +8,62 @@ import msgpack
 from wiked.dump.xml_parser import parse_wiki_dump
 
 
+class DB:
+    def __init__(self, path: Path, mode: str):
+        self.path = path
+        self.mode = mode
+
+    def __enter__(self):
+        self.db = dbm.open(self.path.as_posix(), self.mode)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.db.close()
+
+    def __getitem__(self, key):
+        key: bytes = msgpack.packb(key, use_bin_type=True)
+        return msgpack.unpackb(self.db[key], use_list=True, raw=False)
+
+    def __setitem__(self, key, value):
+        key: bytes = msgpack.packb(key, use_bin_type=True)
+        value: bytes = msgpack.packb(value, use_bin_type=True)
+        self.db[key] = value
+
+
 @click.command()
 @click.argument("filepath", type=click.Path(exists=True))
 def main(filepath):
     filepath = Path(filepath)
     print(f"Processing {filepath.name}.")
     stem = filepath.name.split(".")[0]  # the file extension is .xml.bz2
-    intermediate_database = Path.cwd() / f"{stem}_inter.db"
-    output_file = Path.cwd() / f"{stem}_final.db"
+    by_title_path = Path.cwd() / f"{stem}_by_title.db"
+    by_id_path = Path.cwd() / f"{stem}_by_id.db"
 
-    if intermediate_database.is_file():
-        print("Found existing intermediate database.")
+    if by_title_path.is_file():
+        print("Found existing title -> ID database.")
     else:
-        print("Preparing intermediate database...")
+        print("Preparing title -> ID database...")
         start_timestamp = time()
-        with dbm.open(intermediate_database.as_posix(), "n") as inter_db:
+        with DB(by_title_path, "n") as by_title_db:
             for item in parse_wiki_dump(filepath, skip_links=True):
-                inter_db[item[1]] = str(item[0])
+                by_title_db[item[1]] = int(item[0])
         minutes, seconds = divmod(round(time() - start_timestamp), 60)
         print(f"Elapsed time: {minutes:02d}:{seconds:02d} (m:s). ")
 
-    with dbm.open(intermediate_database.as_posix(), "r") as title_to_id:
-        with dbm.open(output_file.as_posix(), "n") as db:
-            print("Preparing final database...")
+    with DB(by_title_path, "r") as title_to_id:
+        with DB(by_id_path, "n") as by_id_db:
+            print("Preparing ID -> Node database...")
             start_timestamp = time()
             counter = 0
             for item in parse_wiki_dump(filepath):
                 links = dict()
                 for key, value in item[2].items():
                     try:
-                        page_id = int(title_to_id[key])
+                        page_id = title_to_id[key]
                     except KeyError:
                         continue
                     links[page_id] = value
-                db[msgpack.packb(item[0])] = msgpack.packb(
-                    (item[0], item[1], links), use_bin_type=True
-                )
+                by_id_db[item[0]] = (item[0], item[1], links)
                 counter += 1
     minutes, seconds = divmod(round(time() - start_timestamp), 60)
     print(
